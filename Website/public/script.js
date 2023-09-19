fa58e4b2cd8596d25bcbeb2f5ca6ec4414a3cf64
Gurkirat <126689752+Gurkirxt@users.noreply.github.com>
2023-09-19 23:56:08 +0530

    added main.html

--- Website/public/script.js
+++ Website/public/script.js
@@ -6,6 +6,8 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    ,
+    
 const firebaseConfig = {
   apiKey: "AIzaSyCfchBLTflNyZ-P1nOXmMFDSLJAob3j10g",
   authDomain: "tec-park.firebaseapp.com",
